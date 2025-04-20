7fecab9476a87747c4fd33692ccb9de277a50483
Roy-Jin <jinroy@outlook.com>
2025-04-20 14:51:57 +0800

    refactor(页面布局): 移除hitokoto模块并优化语录获取逻辑
    移除index.html中的hitokoto模块，并更新CSS类名以匹配新的sayings模块。重构time.js中的语录获取逻辑，支持从多个API随机获取语录，提升用户体验。

--- script/time.js
+++ script/time.js
@@ -1,21 +1,51 @@
 (async () => {
-    let hitokoto = await fetch("https://v1.hitokoto.cn/")
+    const APIs = [
+        {
+            name: "dujitang",
+            url: "https://v2.xxapi.cn/api/dujitang",
+            format: async (data) => {
+                data = await data.json();
+                return { text: data.data, from: "憨憨语录" }
+            }
+        }, {
+            name: "hitokoto",
+            url: "https://v1.hitokoto.cn/",
+            format: async (data) => {
+                data = await data.json();
+                return { text: await data.hitokoto, from: data.from }
+            }
+        }, {
+            name: "one_yan",
+            url: "https://jkapi.com/api/one_yan",
+            format: async (data) => {
+                return { text: await data.text(), from: "憨憨语录" }
+            }
+        }
+    ]
+
+    // 从APIs数组中随机选择一个API
+    const randomIndex = Math.floor(Math.random() * APIs.length);
+    const randomAPI = APIs[randomIndex];
+
+    let sayings = await fetch(randomAPI.url)
         .catch(() => { return { ok: false } });
-    if (hitokoto.ok) {
-        hitokoto = await hitokoto.json(); 
+    if (sayings.ok) {
+        sayings = await randomAPI.format(sayings);
+        // console.log(`使用${randomAPI.name}API获取语录: ${sayings.text}`);
     } else {
-        hitokoto = { hitokoto: "一言加载失败" }
+        sayings = { text: "语录加载失败！", from: "Roy-Jin" }
     }
+
+    // 更新时间
     setInterval(() => {
-        document.getElementById("time").innerHTML =
-            `
+        document.getElementById("time").innerHTML = `
 <p class="time-text">
     <span class="time-num">${dayjs().format("hh:mm:ss")}</span>
     <span class="time-sub-text">${dayjs().format("A")}</span>
 </p>
 <p class="day-text">${dayjs().format("dddd, D MMMM.")}</p>
 <hr>
-<div class="hitokoto"><div class="text">${hitokoto.hitokoto}</div><div class="from">——《${hitokoto.from}》</div></div>
+<div class="sayings"><div class="text">${sayings.text}</div><div class="from">——《${sayings.from}》</div></div>
 `
     }, 999)
 })()
